7f8cdaac2c58e1b3758d50e2b726fde1299fd37f
luoliang <1812851236@qq.com>
2019-03-14 19:04:12 +0800

    设置了新model的target目录忽略

--- pinyougou_serach_web/target/pinyougou_serach_web-1.0-SNAPSHOT/js/base.js
+++
@@ -1,8 +0,0 @@
-//没有引入其他模块
-var app = angular.module('pinyougou', []);
-/*$sce 服务写成过滤器*/
-app.filter("trustHtml",["$sce",function ($sce) {
-    return function (data) {//传入要被过滤的内容
-        return $sce.trustAsHtml(data);//返回的则是或滤后的内容
-    }
-}]);
