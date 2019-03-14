eec3e8f8310348c224a49613c56cb8fc70f77af1
luoliang <1812851236@qq.com>
2019-03-14 18:41:07 +0800

    这是解耦之前的完整项目activeMQ

--- pinyougou_portal_web/target/pinyougou_portal_web-1.0-SNAPSHOT/js/base_pagination.js
+++
@@ -1,2 +0,0 @@
-//引入了pagination分页模块
-var app=angular.module('pinyougou',['pagination']);
--- pinyougou_serach_web/target/pinyougou_serach_web-1.0-SNAPSHOT/js/service/searchService.js
+++
@@ -1,6 +0,0 @@
-//搜索服务层
-app.service("searchService",function ($http) {
-    this.search=function (searchMap) {
-        return $http.post("itemserach/search.do",searchMap);
-    }
-});
